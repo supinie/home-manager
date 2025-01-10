5f6706631b88059a6f08a391822e986965e6a68a
supinie <86788874+supinie@users.noreply.github.com>
2025-01-10 10:48:49 +0000

    adds ghostty

---
+++ ghostty.nix
@@ -0,0 +1,11 @@
+{
+    enable = true;
+    enableZshIntegration = true;
+    installBatSyntax = true;
+    installVimSyntax = true;
+    settings = {
+        theme = "gruvbox-material";
+        font-size = 11;
+        initial-command = "tmux";
+    };
+}
