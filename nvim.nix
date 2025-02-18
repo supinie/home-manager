5d7f87e5598abd6c5cc5b67bbf73e0d262395222
supinie <86788874+supinie@users.noreply.github.com>
2025-02-18 17:15:10 +0000

    adds coc config

--- nvim.nix
+++ nvim.nix
@@ -66,6 +66,32 @@ in
                 };
             };
         };
+        pluginConfig = ''
+            " Use `[g` and `]g` to navigate diagnostics
+            " Use `:CocDiagnostics` to get all diagnostics of current buffer in location list
+            nmap <silent> [g <Plug>(coc-diagnostic-prev)
+            nmap <silent> ]g <Plug>(coc-diagnostic-next)
+
+            " GoTo code navigation
+            nmap <silent> gd <Plug>(coc-definition)
+            nmap <silent> gy <Plug>(coc-type-definition)
+            nmap <silent> gi <Plug>(coc-implementation)
+            nmap <silent> gr <Plug>(coc-references)
+
+            " Use K to show documentation in preview window
+            nnoremap <silent> K :call ShowDocumentation()<CR>
+
+            function! ShowDocumentation()
+              if CocAction('hasProvider', 'hover')
+                call CocActionAsync('doHover')
+              else
+                call feedkeys('K', 'in')
+              endif
+            endfunction
+
+            " Symbol renaming
+            nmap <leader>rn <Plug>(coc-rename)
+        '';
     };
     withPython3 = true;
     plugins = with pkgs.vimPlugins; [
