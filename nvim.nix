11a5c97505379ba4c5b2412df96bcd2c2ca2dcfa
supinie <86788874+supinie@users.noreply.github.com>
2026-03-04 10:19:58 +0000

    remove cryptobib from telescope

--- nvim.nix
+++ nvim.nix
@@ -297,57 +297,63 @@ in
     noremap T <C-^>
   '';
   extraLuaConfig = ''
-    local config = {
-        options = {
-            icons_enabled = true,
-            theme = 'gruvbox-material',
-            component_separators = "",
-            section_separators = "",
-            disabled_filetypes = {
-                statusline = {},
-                winbar = {},
+        local config = {
+            options = {
+                icons_enabled = true,
+                theme = 'gruvbox-material',
+                component_separators = "",
+                section_separators = "",
+                disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
+                },
+                ignore_focus = {},
+                always_divide_middle = true,
+                globalstatus = false,
+                refresh = {
+                    statusline = 1000,
+                    tabline = 1000,
+                    winbar = 1000,
+                }
             },
-            ignore_focus = {},
-            always_divide_middle = true,
-            globalstatus = false,
-            refresh = {
-                statusline = 1000,
-                tabline = 1000,
-                winbar = 1000,
-            }
-        },
-        sections = {
-            lualine_a = {'mode'},
-            lualine_b = {'branch', 'diff', 'diagnostics'},
-            lualine_c = {'buffers'},
-            lualine_x = {'searchcount'},
-            lualine_y = {'progress'},
-            lualine_z = {'location'}
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = {'filename'},
-            lualine_x = {'location'},
-            lualine_y = {},
-            lualine_z = {}
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
-    }
-    require('lualine').setup(config)
+            sections = {
+                lualine_a = {'mode'},
+                lualine_b = {'branch', 'diff', 'diagnostics'},
+                lualine_c = {'buffers'},
+                lualine_x = {'searchcount'},
+                lualine_y = {'progress'},
+                lualine_z = {'location'}
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {'filename'},
+                lualine_x = {'location'},
+                lualine_y = {},
+                lualine_z = {}
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {}
+        }
+        require('lualine').setup(config)
 
-    require('telescope').setup{
-        defaults = {
-            mappings = {
-                i = {
-                ["<CR>"] = "select_default",
+        require('telescope').setup{
+            defaults = {
+                mappings = {
+                    i = {
+    		    ["<CR>"] = "select_default",
+                    }
+                }
+            },
+            extensions = {
+                bibtex = {
+                    depth = 1,
+                    context = false,
                 }
             }
         }
-    }
-    require"telescope".load_extension("bibtex")
+        require"telescope".load_extension("bibtex")
   '';
 }
